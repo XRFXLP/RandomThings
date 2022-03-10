529b86b861bb52cdda185802ea83a9c59a829104
Ajay Mishra <Ajaym6405@gmail.com>
2022-03-10 18:44:29 +0530

    Update fancy_pstree.py

--- fancy_pstree.py
+++ fancy_pstree.py
@@ -1,16 +1,22 @@
 from subprocess import check_output
 import graphviz
-
+from re import match, split
+from random import randint
 processes = check_output(['ps', '-ax']).strip().split(b'\n')[1:]
 g = graphviz.Digraph('unix', filename='process_tree.gv')
 
-process = []
+process = {}
+colors = {}
 for x in processes:
-    temp = x.decode().strip()
-    temp = temp[:temp.index(' ')]
+    temp = match(r'^\d+ *[^ ]+ *[^ ]+', x.decode().strip()).group()
+    pid, tty, stat = split(r' +', temp)
+    process[pid] = stat
+    current_color = colors[stat] if stat in colors else ('#' + ''.join(hex(randint(0, 255))[2:] for i in range(3)))
+    colors[stat] = current_color
+    g.node(pid, style='filled', color=current_color)
     try:
-        parent_of_temp = check_output(['ps', '-o', 'ppid=', '-p', temp]).decode().strip()
-        g.edge(parent_of_temp, temp)
+        ppid = check_output(['ps', '-o', 'ppid=', '-p', pid]).decode().strip()
+        g.edge(ppid, pid)
     except:
         pass
 
